47bbb594d5c6c67ba7425cf71296866532e979dc
dgarcia <alejandrogarcia-20@hotmail.com>
2024-05-27 19:58:28 -0300

    Probando imagenes

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 
+
+
 let img = {
     width: "15.5rem",
     height: "15.5rem"
@@ -14,7 +16,7 @@ const Item = ({ item }) => {
             <Link to={"/item/" + item.id}>
                 <div className="card h-100 d-flex flex-column align-items-center">
 
-                    <img src={item.urlImagen} className="" style={img} alt={item.nombre} />
+                    <img src={item.urlImagen}  style={img} alt={item.nombre} />
 
                     <div className="card-body">
                         <h5 className="card-text text-uppercase">{item.nombre}</h5>
